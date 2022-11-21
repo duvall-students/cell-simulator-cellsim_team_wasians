58ee08263b15551e74c4cafed97e4cca0f159f4c
blakebb2 <blake.b.byerly@gmail.com>
2022-11-21 11:59:38 -0500

    Update Shark.java

--- src/model/cellObject/Shark.java
+++ src/model/cellObject/Shark.java
@@ -19,12 +19,31 @@ public class Shark extends SeaCreature{
 		return starvingTimeElapsed >= starvingTime;
 	}
 	
-	public CellObject[][] sharkUpdate(int curRow, int curCol, CellObject[][] tempGrid, HashMap<Integer, CellObject> neighbors2) {
+public CellObject[][] sharkUpdate(int curRow, int curCol, CellObject[][] tempGrid, HashMap<Integer, CellObject> neighbors2) {
 		//specific update method
+		//potential hashmap loop method?
 		// if neighbors == fish
 				// move to and delete fish from space
-				
+		for (Entry<Integer, CellObject> entry : neighbors2.entrySet()) {
+			Integer key = entry.getKey();
+		    Object value = entry.getValue();
+		    if (value.equals(instanceof Fish)) {
+		    	value = Shark;
+		    	tempGrid[curRow][curCol].equals(Water);
+		    	break;
+		    }
+		}
 		//else neighbors == water, move there
+		for (Entry<Integer, CellObject> entry : neighbors2.entrySet()) {
+			Integer key = entry.getKey();
+		    Object value = entry.getValue();
+		    if (value.equals(instanceof Water)) {
+		    	value = Shark;
+		    	tempGrid[curRow][curCol].equals(Water);
+		    	break;
+		    }
+		    
+		}
 		
 		return tempGrid;
 	}
