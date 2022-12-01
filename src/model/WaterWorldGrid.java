66d6d718578ce3a7c5ec2f9a08009921d24bc11c
christopher-c-lee <82339562+christopher-c-lee@users.noreply.github.com>
2022-12-01 13:44:00 -0500

    density

--- src/model/WaterWorldGrid.java
+++ src/model/WaterWorldGrid.java
@@ -25,4 +25,8 @@ public class WaterWorldGrid extends Grid {
 			grid[row][column] = getRandomCell();
 		}
 	}
+	
+	public void changeDensity(double fishDensity, double sharkDensity, double waterDensity) {
+		probabilities = new double[] {fishDensity, sharkDensity, waterDensity};
+	}
 }
